2cffa30286561bf7e79d5e5683d8d3ff981ae557
otakuhuang <shyxiaopang@gmail.com>
2025-08-01 01:33:13 +0800

    chore: update nuxt.config.ts to set app title and HTML language attributes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      title: 'Otakuhuang',
+      htmlAttrs: {
+        lang: 'zh-CN',
+      },
+    },
+  },
   compatibilityDate: '2025-07-15',
   modules: ['@nuxt/ui'],
   css: ['@/assets/css/main.css'],
