367765afbb1de6ca27b8afad4797afdd0a659519
otakuhuang <shyxiaopang@gmail.com>
2025-08-01 14:36:14 +0800

    feat: update ESLint configuration with custom rules for quotes, semicolons, and comma dangles

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,13 @@
 // @ts-check
-import withNuxt from './.nuxt/eslint.config.mjs'
+import withNuxt from './.nuxt/eslint.config.mjs';
 
 export default withNuxt(
   // Your custom configs here
-)
+  {
+    rules: {
+      'quotes': ['error', 'single', { avoidEscape: true, allowTemplateLiterals: false }],
+      'semi': ['error', 'always'],
+      'comma-dangle': ['error', 'always-multiline'],
+    },
+  },
+);
